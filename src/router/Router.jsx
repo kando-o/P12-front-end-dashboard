20adf83e9f746f2737344dadc6a77dadd606060c
Jason Kando <jkando@prismamedia.com>
2024-07-22 09:31:42 +0200

    P-12 MAJ ROUTER

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useNavigate, Outlet, useParams, useLocation } from "react-router-dom"
+import Layout from "../layouts/views/layout";
+
 import PageProvider from "../page/pageProvider/views/PageProvider.jsx"
 import PageHome from "../page/home/views/home.jsx"
 import PagePoids from "../components/poids/views/poids.jsx"
@@ -7,22 +9,40 @@ import PageScore from "../components/score/views/score.jsx"
 import PageRadar from "../components/radar/views/radar.jsx"
 import PageObjectif from "../components/objectif/views/objectif.jsx"
 import PageNotFound from "../page/pageNotFound/views/PageNotFound.jsx"
+import PageSettings from "../page/pageSettings/views/pageSettings.jsx"
 
 const Dispatcher = ()=> {
-    console.log("re-route")
     const navigate = useNavigate()
+    const {id} = useParams()
+    const location = useLocation()
+
     useEffect(() => {
-        navigate ("/user/mocked")
-    }, [navigate]) 
-    return <></>
+        if (location.pathname === "/") {
+            navigate ("/user/reglage")
+        }
+        if (id === "mocked") {
+            navigate ("/user/mocked")
+        }
+        if (id === "12") {
+            navigate ("/user/12")
+        }
+        if (id === "19") {
+            navigate ("/user/18")
+        }
+    }, [id, navigate , location.pathname])
+
+    return (
+        <Layout>
+            <Outlet />
+        </Layout>
+    )
 }
 
 const NotFound = () => {
-    console.log("re-route")
+    console.log("not found")
     const navigate = useNavigate()
     useEffect(() => {
         setTimeout(() => {
-            console.log("ps : fuck you asshole")
             navigate ("/user/no_user")
         }, 2500)
     }, [navigate]) 
@@ -34,16 +54,20 @@ const Router = () => {
     <BrowserRouter>
         <Routes>
             {/* connected routes */}
-            <Route path="user/:id" element={<PageProvider />}>
-                <Route index element={<PageHome />} />
-                <Route path="poids" element={<PagePoids />} />
-                <Route path="score" element={<PageScore />} />
-                <Route path="radar" element={<PageRadar />} />
-                <Route path="objectif" element={<PageObjectif />} /> 
-                <Route path="*" index element={<PageNotFound />} />
+            <Route path="/" element={<Dispatcher />}>
+                <Route path="user" element={<PageProvider/>}>
+                    <Route path=":id" element={<PageHome />} />
+                    <Route path="reglage" element={<PageSettings />} /> 
+                    <Route path=":id/poids" element={<PagePoids />} />
+                    <Route path=":id/score" element={<PageScore />} />
+                    <Route path=":id/radar" element={<PageRadar />} />
+                    <Route path=":id/objectif" element={<PageObjectif />} /> 
+                    <Route path="*" element={<PageNotFound />} />
+                </Route>
             </Route>
+
+
             {/* unconnected routes */}
-            <Route index element={<Dispatcher />}/>
             <Route path="*" element={<NotFound />}/>
         </Routes>
     </BrowserRouter>
